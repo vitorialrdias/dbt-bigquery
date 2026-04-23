085dcfcd617a71943794a5fa428165fadc433f2d
Vitoria Dias <vitorialr.dias@gmail.com>
2026-04-23 14:23:27 +0000

    ADD ANALYTICS

--- models/clean_crm_clientes.sql
+++ models/clean_crm_clientes.sql
@@ -5,20 +5,25 @@
     schema='clean'
 ) }}
 
-
 SELECT
     product_id,
     LOWER(IFNULL(title, 'blank')) AS title,
-    CAST(quantity AS INT64) AS quantity,
-    CAST(price AS NUMERIC) AS price,
+    IFNULL(SAFE_CAST(CAST(quantity AS STRING) AS INT64), 0)   AS quantity,
+    IFNULL(SAFE_CAST(CAST(price AS STRING) AS NUMERIC), 0)    AS price,
     IFNULL(
-        CAST(total AS NUMERIC), 
-        CAST(price AS NUMERIC) * CAST(quantity AS NUMERIC)
+        SAFE_CAST(CAST(total AS STRING) AS NUMERIC),
+        SAFE_CAST(CAST(price AS STRING) AS NUMERIC) * SAFE_CAST(CAST(quantity AS STRING) AS NUMERIC)
     ) AS total,
-    CAST(discountPercentage AS NUMERIC) AS discount,
-    IFNULL(CAST(product_discountedTotal AS NUMERIC), 
-        CAST(total AS NUMERIC) - CAST(discountPercentage AS NUMERIC)
+    IFNULL(
+        SAFE_CAST(CAST(discountPercentage AS STRING) AS NUMERIC),
+        ROUND(
+            (1 - SAFE_CAST(CAST(product_discountedTotal AS STRING) AS NUMERIC) 
+            / SAFE_CAST(CAST(total AS STRING) AS NUMERIC)) * 100
+        , 2)
+    ) AS discount,
+    IFNULL(
+    SAFE_CAST(product_discountedTotal AS NUMERIC),
+    CAST(total AS NUMERIC) * (1 - IFNULL(SAFE_CAST(CAST(discountPercentage AS STRING) AS NUMERIC), 0) / 100)
     ) AS total_value
-
 FROM
     `dbt-bigquery-493617.raw.raw_crm_clientes`
